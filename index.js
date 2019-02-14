9a427d06d8a3450814156cbefe374f6d74359d9d
LittleRichKid <33172945+LittleRichKid@users.noreply.github.com>
2019-02-13 18:36:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const Discord = require('discord.js');
 const Bot = new Discord.Client();
 const TOKEN = process.env.BOT_TOKEN;
+const Prefix = '.'
 
 Bot.on('ready', () => {
     console.log('Bot is activated.');
+});
+
+Bot.on('message', message => {
+    const auth = message.author;
+    const user = auth.username;
+    const cont = message.content;
+    const msg = cont.toLowerCase();
+    const chan = message.channel;
+
+    if (msg === Prefix + 'shift') {
+        embed = new Discord.RichEmbed()
+
+        .setTitle('Shift')
+        .setDescription(`A shift is currently being held by ${user}! Come on down for a nice relaxing drink, or for a potentiol promotion!`)
+        .setColor('#007FFF')
+        .setAuthor('Maui')
+
+        chan.send(embed);
+        message.delete();
+    };
+
 });
 
 Bot.login(TOKEN);
