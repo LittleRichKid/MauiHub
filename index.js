aa1f06861a2f0ce6a0e932232c528ff4c947a07d
LittleRichKid <33172945+LittleRichKid@users.noreply.github.com>
2019-02-14 20:09:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,27 @@ Bot.on('message', message => {
         embed = new Discord.RichEmbed()
 
         .setTitle('Shift')
-        .setDescription(`A shift is currently being held by ${user}! Come on down for a nice relaxing drink, or for a potentiol promotion!`)
+        .setDescription(`A shift is currently being held by ${user}! Come on down for a nice relaxing drink, or for a chance to get promoted!`)
         .setColor('#007FFF')
         .setAuthor('Maui')
 
+        chan.send(embed);
+        message.delete();
+    };
+
+    if (msg === Prefix + 'bulk') {
+        chan.bulkDelete(11);
+        auth.send('Bulk deleted 10 messages for you.');
+    };
+
+    if (msg === Prefix + 'interview') {
+        embed = new Discord.RichEmbed()
+
+        .setTitle('Interview')
+        .setDescription('A Interview has started! Head on down to the center for a chance to join our wonderful team! Remember to use grammar!')
+        .setAuthor('Maui')
+        .setColor('#007FFF')
+        
         chan.send(embed);
         message.delete();
     };
